2b5c406ff2755efa38d36674f26ba10c3dddf988
Andres Torres <andrestor2@gmail.com>
2023-05-02 23:54:22 -0500

    Added day12 content.

--- Day12/array-deep-methods.js
+++ Day12/array-deep-methods.js
@@ -1,5 +1,52 @@
 
 let array1 = new Array();
+let array = new Array(1, 2, 3, 4);
+console.log(array);
+
+//convert word letters in array values.
+let myArray = Array.from('hello');
+console.log(myArray);
+
+//Fill n lenght position array with the same value.
+let array2 = Array(5).fill(0);
+console.log(array2);
+
+//is an array?
+console.log(Array.isArray(array2)) //true
+
+//Copy arrays. There are may way to do it but
+//it only performs a shallow copy. 
+//Complex objets won't be copied correctly.
+const originalArray = [1, 2, 3];
+const copiedArray = new Array(...originalArray);
+console.log(originalArray === copiedArray); //false
+
+//Slice
+const copiedArray2 = originalArray.slice();
+console.log(originalArray === copiedArray2); // Output: false
+
+//Concat
+const copiedArray3 = originalArray.concat();
+console.log(originalArray === copiedArray3); // Output: false
 
 
+//Spread operator
+const copiedArray4 = [...originalArray];
+console.log(originalArray === copiedArray4); // Output: false
+
+//Mutidimensional arrays
+const matrix = [];
+matrix[0] = [1, 2, 3];
+matrix[1] = [4, 5, 6];
+matrix[2] = [7, 8, 9];
+
+console.log(matrix);
+// Output: [
+//   [1, 2, 3], 
+//   [4, 5, 6], 
+//   [7, 8, 9]
+// ]
+
+//Deep copy. structuredClone.
+//https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
 
---
+++ Day12/array-deep-methods.part2.js
@@ -0,0 +1,41 @@
+
+//Array.prototype.Every
+//Check if all elements match a condition.
+const numbers = [1, 2, 3, 4, 5];
+const allPositive = numbers.every(function(element) {
+	return element > 0;
+});
+
+console.log(allPositive);
+
+// Output: true
+
+//Check a property
+const objects = [{name: 'John'}, {name: 'Jane'}, {name: 'Jim'}];
+const allHaveName = objects.every(function(element) {
+  return element.hasOwnProperty('name');
+});
+
+console.log(allHaveName);
+// Output: true
+
+//Array.prototype.find
+//Return the first element that match a defined condition.
+//If there is not match, return an undefined value.
+
+const people = [
+    { name: 'John', age: 32 },
+    { name: 'Jane', age: 28 },
+    { name: 'Jim', age: 40 }
+  ];
+  const result = people.find(person => person.age === 32);
+  
+  console.log(result);
+  // Output: { name: 'John', age: 32 }
+
+//Array.prototype.findIndex
+//Return the index of the first element that match a condition.
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const index = numbers2.findIndex(num => num > 5);
+console.log(index); // Output: 5
